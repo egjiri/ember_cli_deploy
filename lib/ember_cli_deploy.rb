6daa5651042303f348fb1b2828a5dffc0219a119
Endri Gjiri <egjiri@gmail.com>
2016-04-30 13:30:35 -0400

    Adds functionality to the gem

--- lib/ember_cli_deploy.rb
+++ lib/ember_cli_deploy.rb
@@ -1,5 +1,39 @@
-require "ember_cli_deploy/version"
+require 'ember_cli_deploy/version'
+require 'redis'
 
 module EmberCliDeploy
-  # Your code goes here...
+  class Index
+    attr_reader :namespace, :redis
+
+    def initialize(namespace, redis_url)
+      @namespace = "#{namespace}:index"
+      @redis = Redis.new(url: redis_url)
+    end
+
+    def revisions
+      redis.zrevrange("#{namespace}:revisions", 0, -1)
+    end
+
+    def latest
+      revisions.first
+    end
+
+    def active
+      redis.get("#{namespace}:current")
+    end
+
+    def activate(version)
+      redis.set("#{namespace}:current", version)
+    end
+
+    def deactivate
+      redis.del("#{namespace}:current")
+    end
+
+    def html(version = nil)
+      version = active if version.blank?
+      version = latest if version == 'latest'
+      redis.get("#{namespace}:#{version}")
+    end
+  end
 end
